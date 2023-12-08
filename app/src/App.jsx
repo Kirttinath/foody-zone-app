bb392d985f65b63816783ae26ed92a9e39ce6a07
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 02:47:45 +0530

    Changed BASE_URL from "http://localhost:9000/" to "http://localhost:9000"

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import SearchResult from './components/SearchResults/SearchResult';
 
-export const BASE_URL = "http://localhost:9000/";
+export const BASE_URL = "http://localhost:9000";
 
 
 
