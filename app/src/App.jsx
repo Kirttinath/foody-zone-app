abe2c1c036a9878594e098502c4621ba672cd4c7
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-08 21:14:22 +0530

    Styled component install

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
 import styled from "styled-components";
-
+import React from "react";
 
 const App = () => {
-  return <Maincontainer>
-    <TopContainer>
-      <div className="logo">
-        <img src="/logo.svg" alt="logo" />
-      </div>
-      <div className="search">
-        <input type="text" placeholder='Seacrh Food'/>
-      </div>
-    </TopContainer>
-  </Maincontainer>
+
+  return (
+    <Maincontainer>
+      <TopContainer>
+        <div className="logo">
+          <img src="/logo.svg" alt="logo" />
+        </div>
+        <div className="search">
+          <input type="text" placeholder='Seacrh Food...' />
+        </div>
+      </TopContainer>
+    </Maincontainer>
+  );
 }
 
-export default App
+export default App;
 
-const Maincontainer = styled.div``;
+const Maincontainer = styled.div`
+
+background-color: #323334;
+`;
 const TopContainer = styled.section``;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+  box-sizing
+}
+
+`
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
