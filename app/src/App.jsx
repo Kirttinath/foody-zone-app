e47f77f7f44195a404995fab6cc4361f803cd6ac
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-08 20:35:39 +0530

    containers added with logo and search bar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,9 +3,19 @@ import styled from "styled-components";
 
 
 const App = () => {
-  return <Maincontainer></Maincontainer>
+  return <Maincontainer>
+    <TopContainer>
+      <div className="logo">
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <div className="search">
+        <input type="text" placeholder='Seacrh Food'/>
+      </div>
+    </TopContainer>
+  </Maincontainer>
 }
 
 export default App
 
 const Maincontainer = styled.div``;
+const TopContainer = styled.section``;
