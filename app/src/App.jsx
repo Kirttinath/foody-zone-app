c961b6d6bf00940a08195e13afe31b4d91e906cf
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-08 20:30:34 +0530

    styled component added

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import styled from "styled-components";
+
 
 const App = () => {
-  return (
-    <div>
-      <h1>Food</h1>
-    </div>
-  )
+  return <Maincontainer></Maincontainer>
 }
 
 export default App
+
+const Maincontainer = styled.div``;
