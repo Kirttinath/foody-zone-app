1654161d5083b6596d9424f4a1ef8acad09a75b5
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-09 02:38:37 +0530

    Update SearchResult component to display food cards

--- app/src/components/SearchResults/SearchResult.jsx
+++ app/src/components/SearchResults/SearchResult.jsx
@@ -1,16 +1,26 @@
 import styled  from "styled-components";
 
-const SearchResult = ({ data }) => {
+const SearchResult = ({ data}) => {
   return (
     <FoodCardContainer>
         <FoodCards>
+     {
+      data?.map((food) => (
+      <FoodCard key={food.name}>
+    
+    <div className="food-image">
+      <img src={food.image} />
+    </div>
+    
 
+      </FoodCard>
+      ))}
         </FoodCards>
       </FoodCardContainer>
   )
 }
 
-export default SearchResult
+export default SearchResult;
 
 const FoodCardContainer = styled.section`
 height: calc(100vh - 210px);
@@ -18,3 +28,5 @@ background-image: url("/bg1.png");
 background-size: cover;
 `;
 const FoodCards = styled.div``;
+
+const FoodCard = styled.div``;
